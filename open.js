368459cb02c1ddd4e5e1e2d5b64ecd41362f2c39
zqz <1726195256@qq.com>
2026-05-02 13:04:25 +0800

    Delete open.js

--- open.js
+++
@@ -1,5 +0,0 @@
-const { exec } = require('child_process');
-const path = require('path');
-
-const htmlPath = path.join(__dirname, 'index.html');
-exec(`start "" "${htmlPath}"`, () => process.exit());
